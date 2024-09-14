5fb6bfd3049432678682ca55f7bc054fdf364fbc
0xNazerrr <lattana-s@hotmail.fr>
2024-09-14 17:06:19 +0200

    :white_check_mark: Result ex

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,58 @@ import { ArrayFn8 } from "./array/ex8";
 import { ArrayFn9 } from "./array/ex9";
 import { ArrayFn10 } from "./array/ex10";
 
+import { ArrayFn30 } from "./array/ex30";
+import { ArrayFn29 } from "./array/ex29";
+import { ArrayFn28 } from "./array/ex28";
+import { ArrayFn27 } from "./array/ex27";
+import { ArrayFn26 } from "./array/ex26";
+import { ArrayFn25 } from "./array/ex25";
+import { ArrayFn24 } from "./array/ex24";
+import { ArrayFn23 } from "./array/ex23";
+import { ArrayFn22 } from "./array/ex22";
+import { ArrayFn21 } from "./array/ex21";
+import { ArrayFn20 } from "./array/ex20";
+
+import { resultFn1 } from "./result/ex1";
+import { resultFn2 } from "./result/ex2";
+import { resultFn3 } from "./result/ex3";
+import { resultFn4 } from "./result/ex4";
+import { resultFn5 } from "./result/ex5";
+import { resultFn6 } from "./result/ex6";
+import { resultFn7 } from "./result/ex7";
+
 function main() {
-  const ex1 = ArrayFn1();
-  const ex2 = ArrayFn2();
-  const ex3 = ArrayFn3();
-  const ex4 = ArrayFn4();
-  const ex5 = ArrayFn5();
-  const ex6 = ArrayFn6();
-  const ex7 = ArrayFn7();
-  const ex8 = ArrayFn8();
-  const ex9 = ArrayFn9();
-  const ex10 = ArrayFn10();
+  // ARRAY
+  // const ex1 = ArrayFn1();
+  // const ex2 = ArrayFn2();
+  // const ex3 = ArrayFn3();
+  // const ex4 = ArrayFn4();
+  // const ex5 = ArrayFn5();
+  // const ex6 = ArrayFn6();
+  // const ex7 = ArrayFn7();
+  // const ex8 = ArrayFn8();
+  // const ex9 = ArrayFn9();
+  // const ex10 = ArrayFn10();
+  // const ex30 = ArrayFn30();
+  // const ex29 = ArrayFn29();
+  // const ex28 = ArrayFn28();
+  // const ex27 = ArrayFn27();
+  // const ex26 = ArrayFn26();
+  // const ex25 = ArrayFn25();
+  // const ex24 = ArrayFn24();
+  // const ex23 = ArrayFn23();
+  // const ex22 = ArrayFn22();
+  // const ex21 = ArrayFn21();
+  // const ex20 = ArrayFn20();
+
+  // RESULT
+  const resultEx1 = resultFn1();
+  const resultEx2 = resultFn2();
+  const resultEx3 = resultFn3();
+  const resultEx4 = resultFn4();
+  const resultEx5 = resultFn5();
+  const resultEx6 = resultFn6();
+  const resultEx7 = resultFn7();
 }
 
 main();
--- src/result/ex4.ts
+++ src/result/ex4.ts
@@ -1,4 +1,17 @@
-// // Exercice 4: Inverser un Result et gérer l'erreurFonctions à utiliser : flip, mapError, recoverObjectif : Inverser les valeurs Ok et Error d'un Result, transformer l'erreur en ajoutant un message personnalisé, puis récupérer une valeur par défaut.
+import { R } from "@mobily/ts-belt";
 
-// const result = R.fromNullable(null, 'Erreur');
-export const resultFn4 = () => {};
+export const resultFn4 = () => {
+  const result = R.fromNullable<null, string>(null, "Erreur");
+
+  const flippedResult = R.flip(result);
+
+  const errorHandledResult = R.mapError(
+    flippedResult,
+    (err: string) => `${err} - Résolu`
+  );
+
+  const finalResult = R.recover(errorHandledResult, "Valeur par défaut");
+  console.log("Résultat final :", finalResult);
+};
+
+resultFn4();
